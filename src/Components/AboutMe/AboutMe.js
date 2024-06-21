0955441ff6b4410367dc2a545b257bd2f475ddc8
Camila Riboldi <cami.riboldi@gmail.com>
2024-06-21 16:22:13 -0300

    tabs setup

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import "./AboutMe.css"
+import React from "react";
+import "./AboutMe.css";
+import tech from "./languages-photo.jpg";
 
 function AboutMe() {
   return (
     <div className="about-me-tab">
-    <div>Skills set</div>
-    <div>Languegues I work with</div>
-  </div>
-  )
+      <div className="side-card">
+        <div className="text">
+          <div className="title">My journey</div>
+          Holding a degree from UBA University, ranked 2nd in the QS Latin
+          America University Rankings 2024 and 71st worldwide. This Bachelor in
+          Information Systems program spans 5 ½ years and offers a comprehensive
+          journey that merges business management with technological innovation.
+          I improved my React skills through courses by Meta and Coderhouse,
+          dedicating over 60 hours to mastering React Js. Additionally, I gained
+          a solid understanding of web development fundamentals, including HTML,
+          CSS, and SASS, through a 38-hour course. From a design perspective, I
+          grasped the fundamentals through a program at the University of
+          Michigan, understanding UX Design from concept to prototype. Backend
+          related, I completed a Coding Bootcamp emphasizing Object-Oriented
+          Programming fundamentals based on Test Driven Development principles,
+          which greatly improved my problem-solving skills and code quality and
+          later on I complemented that with a Python certification from Udemy."
+        </div>
+        <img src={tech} alt="Relevant description" className="image" />
+      </div>
+    </div>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
--- src/Components/Technologies/Tecnologies.js
+++ src/Components/Technologies/Tecnologies.js
@@ -1,11 +1,31 @@
 import React from "react";
+import git from "./git.png";
+import html from "./html.png";
+import scrum from "./scrum.png";
+import css from "./CSS-Logo.png";
+import js from "./JavaScript.png";
+import react from "./react+.png";
+import python from "./python.png";
+
 import "./Tecnologies.css";
 
 function Technologies() {
   return (
     <div className="tecnologies-tab">
-      <div>Skills set</div>
-      <div>Languegues I work with</div>
+      <div className="title">Skills set: What Can I Do</div>
+      <div className="subtitle">
+        I Work With: A Comprehensive Toolkit Encompassing React, HTML, CSS,
+        JavaScript, SQL, and Python.
+      </div>
+      <div className="images">
+        <img src={scrum} alt="Background" className="icon-image" />
+        <img src={git} alt="Background" className="icon-image" />
+        <img src={css} alt="Background" className="icon-image" />
+        <img src={html} alt="Background" className="icon-image" />
+        <img src={js} alt="Background" className="icon-image" />
+        <img src={react} alt="Background" className="icon-image" />
+
+      </div>
     </div>
   );
 }
